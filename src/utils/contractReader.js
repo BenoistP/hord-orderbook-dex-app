c0acb66203454e13ed7e4b2cb79045d55559f56b
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2021-11-17 12:46:58 +0100

    remove unnecessary code

--- src/utils/contractReader.js
+++ src/utils/contractReader.js
@@ -1,68 +1,14 @@
-import { createContract, fetchContracts } from './contractsRegistryService';
-import configJson from '../config/config.json';
-import { wait } from './utilsService';
-import { getValueByMultiProps } from '../utils/mapper';
-
-// used to read data from contract
-let contracts = null;
-
-(async () => {
-  contracts = await fetchContracts();
-})();
-
-const findInContracts = (contractName) => {
-  let newContractsObject = {};
-
-  Object.entries(contracts).forEach(([key, value]) => {
-    const newKey = key.toLocaleLowerCase();
-    newContractsObject = { ...newContractsObject, [newKey]: value };
-  });
-
-  const lowerCasedName = contractName.toLocaleLowerCase();
-  return newContractsObject?.[lowerCasedName];
-};
-
-export const getContractAddress = (contractName) => {
-  if (!contracts) {
-    return getContractAddress(contractName);
-  }
-  const contract = findInContracts(contractName);
-  const contractAddress = getValueByMultiProps(contract?.networks?.[configJson.network], ['Proxy', 'address']);
-  return contractAddress;
-};
-
 export const useContractReader = async (
-  contractName,
+  contract,
   contractFunction,
   parameters = [],
   callMethod = (contractInstance) => contractInstance?.methods?.[contractFunction](...parameters)?.call(),
-  passedAddress,
 ) => {
   let result;
   try {
-    if (!contracts) {
-      return useContractReader(contractFunction, contractName, parameters);
-    }
-    const contractAddress = passedAddress || getContractAddress(contractName);
-
-    const _contract = await createContract(contractName, contractAddress, contracts);
-    const contract = await _contract();
-
     result = await callMethod(contract);
   } catch (error) {
     result = error.message;
   }
   return result;
 };
-
-export const getContractInstance = async (contractName, passedAddress) => {
-  const foundContract = findInContracts(contractName);
-  const address = passedAddress || foundContract?.networks?.[configJson.network]?.['Proxy'];
-
-  const _contract = await createContract(contractName, address, contracts);
-  const contract = await _contract();
-
-  return contract;
-};
-
-export const HORD_TOKEN_ADDRESS = '';
--- src/utils/contractsRegistryService.js
+++ src/utils/contractsRegistryService.js
@@ -1,42 +1,15 @@
-import configJson from 'config/config.json';
-// import contracts from 'config/contracts.json';
-import { handleResponse } from './apiService';
-import { wait } from './utilsService';
+import { contracts } from './contracts';
 
-export const fetchContracts = async () =>
-  fetch(`${configJson.apiPath}/config/contracts`).then((res) => handleResponse(res, false));
-
-// GETTERS
-export const getContractAddress = (name, contracts = {}) => {
-  const address = contracts?.[name]?.networks?.[configJson.network]?.['Proxy'];
-  return address;
+export const createContract = (address, abi) => async () => {
+  return new window._web3.eth.Contract(abi, address);
 };
 
-const findInContracts = (contractName, contracts = {}) => {
-  let newContractsObject = {};
-  Object.entries(contracts).forEach(([key, value]) => {
-    const newKey = key.toLocaleLowerCase();
-    newContractsObject = { ...newContractsObject, [newKey]: value };
+export const loadContracts = (signer) => {
+  const loadedContracts = {};
+  contracts.map((contract) => {
+    loadedContracts[contract.name] = createContract(contract.address, contract.abi);
+    return contract;
   });
-  const lowerCasedName = contractName.toLocaleLowerCase();
-  return newContractsObject?.[lowerCasedName];
-};
 
-export const createContract = (name, address, contracts) => async () => {
-  // this method has been added to avoid search contracts names case-sensitivity
-  const contract = findInContracts(name, contracts);
-  return new window._web3.eth.Contract(contract.abi, address);
-};
-
-export const getErc20Contract = async (address) => createContract('Erc20', address)();
-
-export const hordAddress = getContractAddress('HORD');
-export const hordContract = createContract('HORD', hordAddress);
-export const hordTicketManagerAddress = getContractAddress('HordTicketManager');
-export const HordTicketManagerContract = createContract('HordTicketManager', hordTicketManagerAddress);
-export const hordTicketFactoryAddress = getContractAddress('HordTicketFactory');
-export const HordTicketFactoryContract = createContract('HordTicketFactory', hordTicketFactoryAddress);
-export const HPoolManagerAddress = getContractAddress('HordHpoolManager');
-export const HPoolManagerContract = createContract('HordHpoolManager', HPoolManagerAddress);
-export const hordConfigurationAddress = getContractAddress('HordConfiguration');
-export const HordConfigurationContract = createContract('HordConfiguration', hordConfigurationAddress);
+  return loadedContracts;
+}
