b917f25ca0d8516e1f31bfa1fc1dd0e551b7908f
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2021-11-17 16:54:48 +0100

    create error notification in catch block of contractreader

--- src/utils/contractReader.js
+++ src/utils/contractReader.js
@@ -1,15 +1,19 @@
+import { store } from "index";
+import { createNotification } from "store/actions/uiActions";
+
 export const useContractReader = async (
   contract,
   contractFunction,
   parameters = [],
   callMethod = (contractInstance) =>
-    contractInstance?.methods?.[contractFunction](...parameters)?.call(),
+    contractInstance?.[contractFunction](...parameters)?.call(),
 ) => {
   let result;
   try {
     result = await callMethod(contract);
   } catch (error) {
-    result = error.message;
+    result = { error: error.message };
+    store.dispatch(createNotification('error', 'Something went wrong reading blockchain data.', 4000));
   }
   return result;
 };
