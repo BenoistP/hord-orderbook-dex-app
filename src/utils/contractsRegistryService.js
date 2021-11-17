a22cc26e0a52315b8dcb71e1674198f8157d0edc
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2021-11-17 13:52:28 +0100

    update loadcontracts function

--- src/utils/contractsRegistryService.js
+++ src/utils/contractsRegistryService.js
@@ -1,15 +1,15 @@
 import { contracts } from './contracts';
 
-export const createContract = (address, abi) => async () => {
+const createContract = (address, abi) => async () => {
   return new window._web3.eth.Contract(abi, address);
 };
 
-export const loadContracts = (signer) => {
+export const loadContracts = async () => {
   const loadedContracts = {};
-  contracts.map((contract) => {
-    loadedContracts[contract.name] = createContract(contract.address, contract.abi);
+  await Promise.all(contracts.map(async (contract) => {
+    loadedContracts[contract.name] = await createContract(contract.address, contract.abi)();
     return contract;
-  });
+  }));
 
   return loadedContracts;
 }
