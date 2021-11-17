f873b6e4d822a5ec1acbab06ca3f1cb44236f299
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2021-11-17 15:28:02 +0100

    create orderbook action types

---
+++ src/store/actionTypes/orderbookActionTypes.js
@@ -0,0 +1,6 @@
+export const SET_BUY_ORDERS = 'SET_BUY_ORDERS';
+export const SET_SELL_ORDERS = 'SET_SELL_ORDERS';
+export const SET_TRADE_HISTORY = 'SET_TRADE_HISTORY';
+export const BUY = 'BUY';
+export const SELL = 'SELL';
+export const SET_TRADING_PAIR = 'SET_TRADING_PAIR';
