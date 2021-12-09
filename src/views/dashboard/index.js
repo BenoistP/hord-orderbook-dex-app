83f5747804b6b5797642252a4c6c1128c3d207f6
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2021-12-09 20:53:47 +0100

    add dashboards components

--- src/views/dashboard/index.js
+++ src/views/dashboard/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Dashboard from 'templates/dashboard';
+import Dashboard from './components';
 
 export default function DashboardPage(props) {
   return <Dashboard {...props} />;
