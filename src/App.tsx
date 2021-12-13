ce7b1bdba9e3094e64f781b5332457dbf899f3ba
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2021-12-13 23:46:47 +0100

    add images

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,16 @@ import { ThemeProvider } from 'styled-components'
 import GlobalStyles from './styles/global'
 import theme from './styles/theme'
 import Dashboard from './views/dashboard/components'
+import * as S from './views/landing/styles'
+
 
 function App() {
   return (
      <ThemeProvider theme={theme}>
       <GlobalStyles />
+      <S.Page>
       <Dashboard blockchainApi={'BSC'} account={'0x0'} />
+       </S.Page>
    </ThemeProvider>
   );
 }
