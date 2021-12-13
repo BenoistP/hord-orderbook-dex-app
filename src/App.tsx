3beeeb3e856120523815215af2298c810e35fa37
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2021-12-13 23:17:43 +0100

    add dashboard to add and remove react logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { ThemeProvider } from 'styled-components'
 import GlobalStyles from './styles/global'
 import theme from './styles/theme'
+import Dashboard from './views/dashboard/components'
 
 function App() {
   return (
      <ThemeProvider theme={theme}>
       <GlobalStyles />
+      <Dashboard blockchainApi={'BSC'} account={'0x0'} />
    </ThemeProvider>
   );
 }
