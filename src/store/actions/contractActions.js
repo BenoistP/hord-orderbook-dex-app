4831f7b20515aef17a23998b4752c4784c030e80
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2021-12-15 15:54:43 +0100

    create action to load HPoolToken contract

--- src/store/actions/contractActions.js
+++ src/store/actions/contractActions.js
@@ -1,7 +1,8 @@
 import * as contractActionTypes from '../actionTypes/contractActionTypes';
 import { createNotification } from './uiActions';
 import { unsetBalances } from './balanceActions';
-import { loadContracts } from '../../utils/contractsRegistryService';
+import { createContract, loadContracts } from '../../utils/contractsRegistryService';
+import ERC20 from 'utils/contracts/ERC20.json';
 
 export const connectToContracts = () => async (dispatch) => {
   try {
@@ -10,6 +11,23 @@ export const connectToContracts = () => async (dispatch) => {
     dispatch({
       type: contractActionTypes.SET_CONTRACTS,
       payload: contracts,
+    });
+  } catch (error) {
+    dispatch(createNotification('error', 'Something went wrong loading contract', 4000));
+  }
+};
+
+export const connectToCurrentHPoolTokenContract = (hPoolTokenInfo) => async (dispatch) => {
+  try {
+    const contract = await createContract(hPoolTokenInfo.address, ERC20.abi)();
+    const payload = {
+        address: contract._address,
+        ...contract.methods,
+      };
+
+    dispatch({
+      type: contractActionTypes.SET_CURRENT_HPOOL_TOKEN_CONTRACT,
+      payload,
     });
   } catch (error) {
     dispatch(createNotification('error', 'Something went wrong loading contract', 4000));
