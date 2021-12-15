86c39aabd76a0ae0e429d4d6504b029cbcd2f715
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2021-12-15 19:02:18 +0100

    create setBalance action

--- src/store/actions/balanceActions.js
+++ src/store/actions/balanceActions.js
@@ -1,10 +1,17 @@
+import { useContractReader } from 'utils/contractReader';
+import { WeiToEth } from 'utils/web3Service';
 import * as balanceActionTypes from '../actionTypes/balanceActionTypes';
 
-export const setBalances = (balances) => ({
-  type: balanceActionTypes.GET_BALANCES,
-  payload: balances,
-});
-
+export const setBalance = (contract, tokenName) => async (dispatch, getState) => {
+  const signerAddress = getState().wallet.account;
+  const weiAmountOfTokens = await useContractReader(contract, 'balanceOf', [signerAddress]);
+  const ethAmountOfTokens = WeiToEth(weiAmountOfTokens);
+  dispatch({
+   type: balanceActionTypes.SET_BALANCE,
+   payload: {balance: ethAmountOfTokens, tokenName},
+  });
+}
+  
 export const unsetBalances = () => ({
   type: balanceActionTypes.REMOVE_BALANCES,
 });
