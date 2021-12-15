c56c14453db60665be3f43165b5d541c4667c3ef
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2021-12-15 22:41:55 +0100

    compare addresses with lowercase function

--- src/store/actions/transactionActions.js
+++ src/store/actions/transactionActions.js
@@ -2,7 +2,7 @@ import * as transactionActionTypes from '../actionTypes/transactionActionTypes';
 
 export const setOpenOrders = (orders) => async (dispatch, getState) => {
     const signerAddress = getState().wallet.account;
-    const filteredOpenOrders = orders.filter((order) => order.owner === signerAddress)
+    const filteredOpenOrders = orders.filter((order) => order.owner.toLowerCase() === signerAddress.toLowerCase())
     dispatch({
         type: transactionActionTypes.SET_OPEN_ORDERS,
         payload: filteredOpenOrders
